2b867e1adec16268e169c974fab695610ab3e441
gabriel.goes.08.dev@gmail.com <gabriel.goes.08.dev@gmail.com>
2026-03-19 09:44:46 -0300

    Nome adicionado

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 int main(){
     printf("Manuela Cespede!"); //adicionado o primeiro print
     printf("Marcos Peterson");//adicionado meu primeiro print
+    printf("Gabriel Góes");
     printf("\n");
     return 0;
 }
