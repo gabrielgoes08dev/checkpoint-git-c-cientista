57a98c8718fe0749af5a1bac8d59a906bfc5bdbe
manuelacespede-crtl <manu.cespede.tech@gmail.com>
2026-03-19 09:37:24 -0300

    código modificado e atualizado com comentário

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include<stdio.h> // incluindo biblioteca
 int main(){
-    printf("Manuela Cespede!");
+    printf("Manuela Cespede!"); //adicionado o primeiro print
     printf("Marcos Peterson");//adicionado meu primeiro print
     printf("\n");
     return 0;
