e794fed620ec22df8ed64c798095a0b463b784c1
Laboratório FIAP <marcospetersonmartins@gmail.com>
2026-03-19 09:30:31 -0300

    adicionado o gitignore

--- main.c
+++ main.c
@@ -1,1 +1,8 @@
 #include<stdio.h> // incluindo biblioteca
+int main(){
+    printf("Manuela Cespede!");
+    printf("Marcos Peterson");//adicionado meu primeiro print
+    printf("\n");
+    return 0;
+}
+
